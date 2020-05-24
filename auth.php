a673e99a73df4bc650885c2edb3d85966731f2d3
noe91 <noeliafonsil@gmail.com>
2020-05-24 23:52:33 +0200

    switch -> AuthController

--- auth.php
+++ auth.php
@@ -1,48 +1,14 @@
 <?php
-require_once './class/User.php';
+require_once './controllers/AuthController.php';
 
-$route = '';
+$action = '';
 
 if (isset($_GET['action'])) {
-    $route = $_GET['action'];
+    $action = $_GET['action'];
 }
 
-function insertData($email, $pass) {
-    $user = new User();
-    $user->email = $email;
-    $user->pass = $pass;
+$controller = new AuthController();
 
-    return $user;
-}
-
-
-switch ($route) {
-    case 'register':
-        insertData($_POST['email'], hash('sha256', $_POST['password']));
-
-        if ($user->createUser() === TRUE) {
-            header('Location: index.php?r=login');
-        } else {
-            header('Location: index.php?r=register');
-        }
-
-        break;
-
-    case 'login':
-        $user = insertData($_POST['email'], hash('sha256', $_POST['password']));
-
-        $result = $user->userLogin();
-
-        if ($result !== FALSE) {
-            setcookie('auth', 1, time() + (86400 * 30), "/");
-            setcookie('user', $result['id'], time() + (86400 * 30), "/");
-
-            header('Location: index.php?r=panel');
-        } else {
-            setcookie('auth', 0, time() + (86400 * 30), "/");
-
-            header('Location: index.php?r=login');
-        }
-
-        break;
+if(method_exists($controller, $action)){
+    $controller->$action();
 }
---
+++ controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+require_once './class/User.php';
+
+class AuthController
+{
+    private function insertData($email, $pass) {
+        $user = new User();
+        $user->email = $email;
+        $user->pass = $pass;
+    
+        return $user;
+    }
+
+    public function register()
+    {
+        $user = $this->insertData($_POST['email'], hash('sha256', $_POST['password']));
+
+        if ($user->createUser() === TRUE) {
+            header('Location: index.php?r=login');
+        } else {
+            header('Location: index.php?r=register');
+        }
+    }
+
+    public function login()
+    {
+        $user = $this->insertData($_POST['email'], hash('sha256', $_POST['password']));
+
+        $result = $user->userLogin();
+
+        if ($result !== FALSE) {
+            setcookie('auth', 1, time() + (86400 * 30), "/");
+            setcookie('user', $result['id'], time() + (86400 * 30), "/");
+
+            header('Location: index.php?r=panel');
+        } else {
+            setcookie('auth', 0, time() + (86400 * 30), "/");
+
+            header('Location: index.php?r=login');
+        }
+    }
+}
